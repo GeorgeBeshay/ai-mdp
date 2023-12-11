cbfff9850ce59a8aff4a8311d5eba7895e359757
Maria Bassem <maria.bassem2002@gmail.com>
2023-12-11 10:14:52 +0200

    update some print statements
    Taking the reward after exiting the state and making the V_k_terminal states = board[i][j]

--- mdp-src/algorithms/value_iteration.py
+++ mdp-src/algorithms/value_iteration.py
@@ -18,7 +18,7 @@ def get_successors(currX, currY, action, row_length=3, col_length=3) -> List[Tup
 
     sol = []
     for (i, j) in x:
-        if currX + i < row_length and currY + j < col_length:
+        if 0 <= currX + i < row_length and 0 <= currY + j < col_length:
             sol.append((currX + i, currY + j))
         else:
             sol.append((currX, currY))
@@ -28,11 +28,11 @@ def get_successors(currX, currY, action, row_length=3, col_length=3) -> List[Tup
 # print(get_successors(1, 1, 'N'))
 
 
-def is_terminal_state(board, i, j):
+def is_terminal_state(board, i, j) -> bool:
     return board[i][j] != -1
 
 
-def are_equal(V_k, V_k_next, epsilon=1e-6) -> bool:
+def check_policy_convergence(V_k: List[List[float]], V_k_next: List[List[float]], epsilon: float = 1e-6) -> bool:
     # Check if the dimensions are the same
     if len(V_k) != len(V_k_next) or any(len(row) != len(V_k_next[0]) for row in V_k):
         return False
@@ -72,15 +72,15 @@ def value_iteration(board: List[List[int]], V_k: List[List[float]], gamma=0.99, 
                     # print("successor is = ", successor)
                     transition = 0.8 if first_successor else 0.1
                     sum += transition * (board[i][j] + gamma * V_k[actX][actY])
-                    # print(f"sum += {transition} * (board[{actX}][{actY}] + gamma * V_k[{actX}][{actY}])"
-                    #       f" = sum += {transition} * ({board[actX][actY]} + {gamma} * {V_k[actX][actY]})")
+                    # print(f"sum += {transition} * (board[{i}][{j}] + gamma * V_k[{actX}][{actY}])"
+                    #       f" = sum += {transition} * ({board[i][j]} + {gamma} * {V_k[actX][actY]})")
                     # print("sum = ", sum)
                     first_successor = False
                 if sum > max_next_V:
                     max_next_V = sum
                     max_arg_s = action
-            # print(f"max sum for state = board[{i}][{j}] is {max_next_V} and action is '{max_arg_s}'")
-            V_k_next[i][j] = max_next_V
+            # print(f"max sum for state = board[{i}][{j}] is {round(max_next_V, 2)} and action is '{max_arg_s}'")
+            V_k_next[i][j] = round(max_next_V, 2)
             arg_max[i][j] = max_arg_s
 
     for row in V_k_next:
@@ -88,14 +88,14 @@ def value_iteration(board: List[List[int]], V_k: List[List[float]], gamma=0.99, 
     for row in arg_max:
         print(" ".join(row))
 
-    if are_equal(V_k, V_k_next):
+    if check_policy_convergence(V_k, V_k_next):
         return V_k_next, arg_max
     else:
-        return value_iteration(board, V_k_next,k=k+1)
+        return value_iteration(board, V_k_next, k=k + 1)
 
 
 # Initialize board and V_k
-example_board = [[0, -1, 10], [-1, -1, -1], [-1, -1, -1]]
+example_board = [[100, -1, 10], [-1, -1, -1], [-1, -1, -1]]
 V_0 = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
 
 # Call the value_iteration function
