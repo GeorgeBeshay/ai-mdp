aaf87fd9bd297cf22c2c69c134d9479986a1062d
Maria Bassem <maria.bassem2002@gmail.com>
2023-12-11 11:40:46 +0200

    Taking r as input, removing some comments and renaming some methods

--- mdp-src/algorithms/value_iteration.py
+++ mdp-src/algorithms/value_iteration.py
@@ -1,8 +1,19 @@
 from typing import List, Tuple
-# import numpy as np
 
 
-def get_successors(currX, currY, action, row_length=3, col_length=3) -> List[Tuple[int, int]]:
+def get_next_moves(currX: int, currY: int, action: str, row_length: int = 3, col_length: int = 3) -> List[
+    Tuple[int, int]]:
+    """
+        Get the next moves based on the current position and action.
+
+        :param currX: Current X-coordinate.
+        :param currY: Current Y-coordinate.
+        :param action: Action ('N', 'W', 'S', 'E').
+        :param row_length: Number of rows in the world.
+        :param col_length: Number of columns in the world.
+
+        :return: List of tuples representing the next moves.
+    """
     # N - W - S - E
     x = []
     if action == 'N':
@@ -14,25 +25,40 @@ def get_successors(currX, currY, action, row_length=3, col_length=3) -> List[Tup
     elif action == 'E':
         x = [(0, 1), (1, 0), (-1, 0)]
     else:
-        return []  # error should not occur
+        return []  # error: This should not occur
 
-    sol = []
+    next_moves = []
     for (i, j) in x:
         if 0 <= currX + i < row_length and 0 <= currY + j < col_length:
-            sol.append((currX + i, currY + j))
+            next_moves.append((currX + i, currY + j))
         else:
-            sol.append((currX, currY))
-    return sol
+            next_moves.append((currX, currY))
+    return next_moves
 
 
-# print(get_successors(1, 1, 'N'))
+def is_terminal_state(rewards: List[List[int]], i: int, j: int) -> bool:
+    """
+        Check if the given position represents a terminal state in the world.
+
+        :param rewards: 2D list representing the game world.
+        :param i: Row index.
+        :param j: Column index.
+
+        :return: True if the position is a terminal state, False otherwise.
+        """
+    return rewards[i][j] != -1
 
 
-def is_terminal_state(board, i, j) -> bool:
-    return board[i][j] != -1
+def check_value_convergence(V_k: List[List[float]], V_k_next: List[List[float]], epsilon: float = 1e-6) -> bool:
+    """
+        Check if the values have converged between two iterations.
 
+        :param V_k: Current values.
+        :param V_k_next: Values from the next iteration.
+        :param epsilon: Convergence threshold.
 
-def check_policy_convergence(V_k: List[List[float]], V_k_next: List[List[float]], epsilon: float = 1e-6) -> bool:
+        :return: True if the values have converged, False otherwise.
+        """
     # Check if the dimensions are the same
     if len(V_k) != len(V_k_next) or any(len(row) != len(V_k_next[0]) for row in V_k):
         return False
@@ -42,70 +68,88 @@ def check_policy_convergence(V_k: List[List[float]], V_k_next: List[List[float]]
         for j in range(len(V_k[0])):
             if abs(V_k[i][j] - V_k_next[i][j]) > epsilon:
                 return False
-
     return True
 
 
-def value_iteration(board: List[List[int]], V_k: List[List[float]], gamma=0.99, k=0) -> Tuple[List[List[float]], List[List[str]]]:
-    print(f"================================================= k = {k} =================================================")
-    num_rows = len(board)
-    num_columns = len(board[0])
+def value_iteration(world: List[List[int]], V_k: List[List[float]], gamma: float = 0.99, k: int = 0) -> Tuple[List[List[float]], List[List[str]]]:
+    """
+        Perform value iteration to calculate the optimal values and policies for each state.
+
+        :param world: 2D list representing the game world.
+        :param V_k: Current values.
+        :param gamma: Discount factor.
+        :param k: Iteration count.
+
+        :return: Tuple containing the updated values and optimal policies.
+        """
+    # print(f"================================================= k = {k} =================================================")
+    num_rows = len(world)
+    num_columns = len(world[0])
     V_k_next = [[0] * num_columns for _ in range(num_rows)]
     arg_max = [[''] * num_columns for _ in range(num_rows)]
 
     for i in range(num_rows):
         for j in range(num_columns):
-            if is_terminal_state(board, i, j):  # You need to define is_terminal_state
-                V_k_next[i][j] = board[i][j]  # Terminal state value
+            if is_terminal_state(world, i, j):
+                V_k_next[i][j] = world[i][j]  # Terminal state value
                 arg_max[i][j] = 'R'  # No action for terminal state
                 continue
             max_next_V = -float('inf')
             max_arg_s = None
             for action in ['N', 'W', 'S', 'E']:
-                sum = 0.0
-                first_successor = True
                 # print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
                 # print(f"i = {i}, j = {j}")
                 # print("action is = ", action)
-                for successor in get_successors(i, j, action):
+                sum = 0.0
+                first_successor = True
+                for successor in get_next_moves(i, j, action):
                     (actX, actY) = successor
                     # print("successor is = ", successor)
                     transition = 0.8 if first_successor else 0.1
-                    sum += transition * (board[i][j] + gamma * V_k[actX][actY])
-                    # print(f"sum += {transition} * (board[{i}][{j}] + gamma * V_k[{actX}][{actY}])"
-                    #       f" = sum += {transition} * ({board[i][j]} + {gamma} * {V_k[actX][actY]})")
+                    sum += transition * (world[i][j] + gamma * V_k[actX][actY])
+                    # print(f"sum += {transition} * (world[{i}][{j}] + gamma * V_k[{actX}][{actY}])"
+                    #       f"\n= sum += {transition} * ({world[i][j]} + {gamma} * {V_k[actX][actY]})")
                     # print("sum = ", sum)
                     first_successor = False
                 if sum > max_next_V:
                     max_next_V = sum
                     max_arg_s = action
-            # print(f"max sum for state = board[{i}][{j}] is {round(max_next_V, 2)} and action is '{max_arg_s}'")
-            V_k_next[i][j] = round(max_next_V, 2)
+            # print(f"max sum for state = world[{i}][{j}] is {round(max_next_V, 2)} and action is '{max_arg_s}'")
+            V_k_next[i][j] = max_next_V
             arg_max[i][j] = max_arg_s
 
-    for row in V_k_next:
-        print(" ".join("{:.2f}".format(value) for value in row))
-    for row in arg_max:
-        print(" ".join(row))
+    # for row in V_k_next:
+    #     print(" ".join("{:.2f}".format(value) for value in row))
+    # for row in arg_max:
+    #     print(" ".join(row))
 
-    if check_policy_convergence(V_k, V_k_next):
+    if check_value_convergence(V_k, V_k_next):
         return V_k_next, arg_max
     else:
-        return value_iteration(board, V_k_next, k=k + 1)
+        return value_iteration(world, V_k_next, k=k + 1)
 
 
-# Initialize board and V_k
-example_board = [[100, -1, 10], [-1, -1, -1], [-1, -1, -1]]
-V_0 = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+while True:
+    r = float(input("Enter the value of r (enter -1 to exit): "))
+    if r == -1:
+        break
 
-# Call the value_iteration function
-V_k, pi_k = value_iteration(example_board, V_0)
-# Print V_k
-print("V_k =")
-for row in V_k:
-    print(" ".join("{:.2f}".format(value) for value in row))
+    # Initialize world and V_0
+    example_world = [[r, -1, 10], [-1, -1, -1], [-1, -1, -1]]
+    V_0 = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
 
-# Print pi_k
-print("pi_k =")
-for row in pi_k:
-    print(" ".join(row))
+    print("Current Rewards =")
+    for row in example_world:
+        print(" ".join("{:.2f}".format(value) for value in row))
+
+    # Call the value_iteration function
+    V_k, pi_k = value_iteration(example_world, V_0)
+    # Print V_k
+    print("V_k =")
+    for row in V_k:
+        print(" ".join("{:.2f}".format(value) for value in row))
+
+    # Print pi_k
+    print("pi_k =")
+    for row in pi_k:
+        print(" ".join(row))
